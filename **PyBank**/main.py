95221ea4043c4a0fbb6057d78e8de98a4a87ddc5
Eric Delgado <delgado.eric90@gmail>
2018-03-11 20:56:21 -0700

    [Finished]

--- **PyBank**/main.py
+++ **PyBank**/main.py
@@ -1,4 +1,3 @@
-import os
 import csv
 
 #Import the data
@@ -35,3 +34,47 @@ totalrevenue = int(0)
 for x in revenues:
 
     totalrevenue = totalrevenue + x
+    
+
+#Create a list that holds the differences bewteen months
+
+deltas =[]
+
+for x in range(1,len(revenues)):
+    
+     deltas.append(revenues[x] - revenues[x-1])
+
+#Identify average, maximum and minimum values and associated months among the deltas
+
+average = sum(deltas)/len(deltas)
+
+maximum = max(deltas)
+
+maxmonth = months[deltas.index(maximum)+1]
+
+minimum = min(deltas)
+
+minmonth = months[deltas.index(minimum)+1]
+
+#Print the results
+
+print("Financial Analysis")
+print("Total Months: " + str(monthsnumber))
+print("Total Revenue: $" + str(totalrevenue))
+print("Average Revenue Change: $" + str(average))
+print("Greatest Increase In Revenue: " + str(maxmonth) + " ($" + str(maximum) + ")")
+print("Greatest Decrease In Revenue: " + str(minmonth) + " ($" + str(minimum) + ")")
+
+#Write the results to a file
+
+with open('results.csv', 'w') as csvfile:    
+    results = csv.writer(csvfile, delimiter= ',')
+    results.writerow('Financial Analysis')
+    results.writerow("Total Months: " + str(monthsnumber))
+    results.writerow("Total Revenue: $" + str(totalrevenue))
+    results.writerow("Average Revenue Change: $" + str(average))
+    results.writerow("Greatest Increase In Revenue: " + str(maxmonth) + " ($" + str(maximum) + ")")
+    results.writerow("Greatest Decrease In Revenue: " + str(minmonth) + " ($" + str(minimum) + ")")
+    
+    
+    
